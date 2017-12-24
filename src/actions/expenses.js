3eb03b328f6aa453131afec6cd7ad6b403816e66
nazmulsisil <sisil8sisil@gmail.com>
2017-12-24 18:18:49 +0600

    add startEditExpense

--- src/actions/expenses.js
+++ src/actions/expenses.js
@@ -43,6 +43,8 @@ export const startRemoveExpense = ({ id } = {}) => {
 	};
 };
 
+
+
 //////////////////
 // EDIT_EXPENSE //
 //////////////////
@@ -51,6 +53,14 @@ export const editExpense = (id, updates) => ({
 	id,
 	updates
 });
+
+export const startEditExpense = (id, updates) => {
+	return (dispatch) => {
+		return database.ref(`expenses/${id}`).update(updates).then(()=>{
+			dispatch(editExpense(id, updates));
+		});
+	};
+};
 
 // SET_EXPENSES
 
