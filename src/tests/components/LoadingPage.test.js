9f2c4045d2bfdde927168af109f78d606ae14c17
nazmulsisil <sisil8sisil@gmail.com>
2017-12-26 15:32:24 +0600

    Add Expensify styles

---
+++ src/tests/components/LoadingPage.test.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { shallow } from 'enzyme';
+import LoadingPage from '../../components/LoadingPage';
+
+test('should render correctly loadingPage', () => {
+	const wrapper = shallow(<LoadingPage />);
+	expect(wrapper).toMatchSnapshot();
+});
